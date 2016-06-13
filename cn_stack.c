fcbb35bee43007dcb179a6697c22d9e1d4150313
Clara Taylor <idesty@derpg.xyz>
2016-06-13 18:37:07 -0400

    Added library

---
+++ cn_stack.c
@@ -0,0 +1,118 @@
+/*
+ * CN_Stack Library
+ *
+ * Version 1.0.0 (Last Updated 2016-06-05)
+ *
+ * Description:
+ *     Stacks for C library. Implements the data structure with a main struct
+ *     and a node struct with pointers to the top node only. Any datatype can
+ *     be stored in a CN_Stack, just like C++ Stacks.
+ *
+ *     Changelog of library is located at the bottom of "cn_stack.h"
+ *
+ * Author:
+ *     Clara Van Nguyen
+ *
+ * For documentation and details on every function in this library, visit the following URL:
+ * http://web.eecs.utk.edu/~ssmit285/lib/cn_stack/index.html
+ */
+
+#include "cn_stack.h"
+
+//Functions
+//Initializer
+CN_STACK new_cn_stack(cns_uint size) {
+	CN_STACK stack = (CN_STACK) malloc(sizeof(struct cn_stack));
+	stack->size = 0;
+	stack->head = NULL;
+	stack->elem_size = size;
+
+	return stack;
+}
+
+//Add
+void cn_stack_push(CN_STACK stack, void* ptr) {
+	//Pushes an element to the end of the stack (Constant time implementation).
+	CNS_NODE* data = __cn_stack_create_node(ptr, stack->elem_size);
+	if (stack->size == 0)
+		stack->head = (void *)data;
+	else {
+        data->next = stack->head;
+        stack->head = data;
+		//stack->back->next = (void *) data;
+    }
+
+	//This element is guaranteed to be the last element.
+    if (stack->size == 0)
+	   stack->back = data;
+
+	//Increment the size
+	stack->size++;
+}
+
+//Modify
+void cn_stack_clear(CN_STACK stack) {
+	//Clears the stack
+	while (stack->size != 0)
+		cn_stack_pop(stack);
+}
+
+void cn_stack_pop(CN_STACK stack) {
+	//Removes the first entry from the stack.
+	CNS_NODE* data = stack->head;
+
+	if (stack->size == 1)
+		stack->head == NULL;
+	else
+		stack->head = (void *) stack->head->next;
+
+	__cn_stack_free_node(data);
+
+	stack->size--;
+}
+
+//Get
+void* cn_stack_front(CN_STACK stack) {
+	return stack->head->data;
+}
+
+void* cn_stack_back(CN_STACK stack) {
+	return stack->back->data;
+}
+
+cns_uint cn_stack_size(CN_STACK stack) {
+	return stack->size;
+}
+
+cns_uint cn_stack_element_size(CN_STACK stack) {
+	return stack->elem_size;
+}
+
+cns_byte cn_stack_empty(CN_STACK stack) {
+	return (stack->size == 0);
+}
+
+//Deinitializer
+void cn_stack_free(CN_STACK stack) {
+	cn_stack_clear(stack);
+	free(stack);
+}
+
+//Functions you won't use if you are sane
+CNS_NODE* __cn_stack_create_node(void* ptr, cns_uint elem_size) {
+	CNS_NODE* node = (CNS_NODE *) malloc(sizeof(struct cns_node));
+
+	//Default Values
+	node->next = NULL;
+	node->data = (void *) malloc(elem_size);
+
+	//Assign data to the struct with information from the stack class
+	memcpy(node->data, ptr, elem_size);
+
+	return node;
+}
+
+void __cn_stack_free_node(CNS_NODE* ptr) {
+	free(ptr->data);
+	free(ptr);
+}
---
+++ cn_stack.h
@@ -0,0 +1,97 @@
+/*
+ * CN_Stack Library
+ *
+ * Version 1.0.0 (Last Updated 2016-06-05)
+ *
+ * Description:
+ *     Stacks for C library. Implements the data structure with a main struct
+ *     and a node struct with pointers to the top node only. Any datatype can
+ *     be stored in a CN_Stack, just like C++ Stacks.
+ *
+ *     Changelog of library is located at the bottom of "cn_stack.h"
+ *
+ * Author:
+ *     Clara Van Nguyen
+ *
+ * For documentation and details on every function in this library, visit the following URL:
+ * http://web.eecs.utk.edu/~ssmit285/lib/cn_stack/index.html
+ */
+
+#ifndef __CN_stack__
+#define __CN_stack__
+
+#include <stdlib.h>
+#include <string.h>
+
+//Custom Types
+typedef unsigned int       cns_uint;
+typedef unsigned long long cns_u64;
+typedef unsigned char      cns_byte;
+
+//Structs
+typedef struct cns_node {
+	struct cns_node* next;
+	void           * data;
+} CNS_NODE;
+
+typedef struct cn_stack {
+	//Nodes holding data
+	CNS_NODE* head,
+		    * back;
+
+	//Data needed in the structure
+	cns_uint  size,
+	          elem_size;
+} *CN_STACK;
+
+typedef CN_STACK     QUEUE;    //For you C++ people...
+#define CNS_ITERATOR CNS_NODE* //My point still stands...
+
+//Functions
+//Initializer
+CN_STACK  new_cn_stack          (cns_uint);
+
+//Add
+void      cn_stack_push         (CN_STACK, void*);
+
+//Modify
+void      cn_stack_clear        (CN_STACK);
+void      cn_stack_pop          (CN_STACK);
+
+//Get
+void*     cn_stack_front        (CN_STACK);
+void*     cn_stack_back         (CN_STACK);
+cns_uint  cn_stack_size         (CN_STACK);
+cns_uint  cn_stack_element_size (CN_STACK);
+cns_byte  cn_stack_empty        (CN_STACK);
+
+//Deinitializer
+void      cn_stack_free         (CN_STACK);
+
+//Functions you won't use if you are sane
+CNS_NODE* __cn_stack_create_node(void*, cns_uint);
+void      __cn_stack_free_node  (CNS_NODE*);
+
+//Macros
+#define cn_stack_init(type) \
+	new_cn_stack(sizeof(type)) //For those who hate typing sizeof(...)i
+
+#define cn_stack_deref(iterator, type) \
+	*(type *)iterator
+
+#define cn_stack_deref_front(CN_STACK, type) \
+	*(type *)CN_STACK->head->data
+
+#define cn_stack_deref_back(CN_STACK, type) \
+	*(type *)CN_STACK->back->data
+
+#endif
+
+/***************************************\
+    * ** ***    CHANGELOG    *** ** *
+\***************************************/
+
+/*
+    2016-06-05 (1.0.0)
+      - Started writing CN_Queue. Initial (And probably the only) Release.
+*/
